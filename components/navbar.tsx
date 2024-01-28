68e70d28e8b000cab1afb045fbe32b7243abff9c
druvkotwani <druvkotwani12@gmail.com>
2024-01-28 18:19:06 +0530

    navbar icon size increased

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -61,8 +61,8 @@ export const Navbar = () => {
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="1.2em"
-              height="1.2em"
+              width="24px"
+              height="24px"
               viewBox="0 0 256 256"
             >
               <path
