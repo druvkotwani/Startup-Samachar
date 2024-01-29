55fbb55ae2d4f6a2d97b2c8514d073f25c6435d7
druvkotwani <druvkotwani12@gmail.com>
2024-01-29 15:25:08 +0530

    changed font and reduced padding top

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,10 +41,10 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className="container mx-auto max-w-7xl pt-8 px-6 flex-grow ">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
+            <footer className="w-full flex items-center justify-center py-3 SourceCodePro">
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
--- components/Button.jsx
+++ components/Button.jsx
@@ -7,7 +7,7 @@ export default function ButtonComponent({ href }) {
     return (
         <Link href={href} target="_blank" rel="norefferrer" className="flex gap-4 items-center">
             <Button href="https://example.com/news" className="h-8 rounded-md" color="default" endContent={<GoTo />} variant="bordered" >
-                <span className="font-medium ">
+                <span className="font-semibold SourceCodePro ">
                     Read News
                 </span>
             </Button>
