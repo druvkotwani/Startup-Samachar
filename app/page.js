daaa744ca449041eb61c99127f0bac424fc65778
druvkotwani <druvkotwani12@gmail.com>
2024-01-29 16:22:06 +0530

    added skeletons

--- app/page.js
+++ app/page.js
@@ -2,10 +2,11 @@
 
 import { useEffect, useState } from "react";
 import EachCard from "../components/Card";
+import SkeletonComponent from "../components/Skeleton";
 
 export default function Home() {
   const [news, setNews] = useState([])
-
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     fetch('https://startup-samachar-api.vercel.app/news')
       .then(res => res.json())
@@ -13,24 +14,33 @@ export default function Home() {
         // Shuffle the data array
         const shuffledData = data.sort(() => Math.random() - 0.5);
         setNews(shuffledData);
+        setLoading(false)
       });
   }, [])
 
   return (
+    <>
 
-    <div className="min-h-screen flex flex-col mx-auto max-w-screen-xl px-2 sm:px-6 lg:px-6">
-      <div className="grid grid-cols-1 gap-6 py-4 ">
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="min-h-screen flex flex-col mx-auto max-w-screen-xl px-2 sm:px-6 lg:px-6">
+        <div className="grid grid-cols-1 gap-6 py-4 ">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 
-          {
-            news.map((function (item, index) {
-              return <EachCard key={index} heading={item.title} source={item.source} imgSrc={item.imgSrc} href={item.href} />
-            }))
-          }
+            {
+              loading ? <>
+                {Array.from({ length: 12 }).map((_, index) => {
+                  return <SkeletonComponent key={index} />
+                })}
+              </>
+                :
+                news.map((function (item, index) {
+                  return <EachCard key={index} heading={item.title} source={item.source} imgSrc={item.imgSrc} href={item.href} />
+                }))
+            }
 
-          {/* <EachCard src='inc42' heading='BNP Paribas On Block Deal Spree, Sells Shares Of Paytm & Policybazaar' by='Team Inc42' img='https://inc42.com/wp-content/uploads/2024/01/bn-zomato-featured--490x367.png' /> */}
+            {/* <EachCard src='inc42' heading='BNP Paribas On Block Deal Spree, Sells Shares Of Paytm & Policybazaar' by='Team Inc42' img='https://inc42.com/wp-content/uploads/2024/01/bn-zomato-featured--490x367.png' /> */}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ components/Skeleton.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Card, CardBody, CardHeader, Skeleton } from "@nextui-org/react";
+
+export default function SkeletonComponent() {
+    return (
+        <Card className="py-4 hover:scale-105 ">
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                <div className="flex items-center justify-evenly    w-full gap-4 mb-2">
+                    <div>
+                        <Skeleton className="flex rounded-full w-12 h-12" />
+                    </div>
+                    <div className="w-full flex flex-col gap-2">
+                        <Skeleton className="h-3 w-3/5 rounded-lg" />
+                    </div>
+                </div>
+                <div className="w-full flex flex-col gap-2 mt-3">
+                    <Skeleton className="h-3 w-1/5 rounded-lg" />
+                    <Skeleton className="h-3 w-2/5 rounded-lg" />
+                    <Skeleton className="h-3 w-3/5 rounded-lg" />
+                    <Skeleton className="h-3 w-4/5 rounded-lg" />
+                    <Skeleton className="h-3 w-full rounded-lg" />
+                </div>
+            </CardHeader>
+
+            <div className="flex items-center justify-center overflow-visible pt-8 pb-2">
+                <Card className="mx-auto w-[238px]  space-y-5 p-4" radius="lg">
+                    <Skeleton className="rounded-lg">
+                        <div className="h-32  rounded-lg bg-default-300"></div>
+                    </Skeleton>
+                </Card>
+            </div>
+            <div className="flex items-center gap-3 mx-auto">
+                <Skeleton className="w-3/5 rounded-lg">
+                    <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+                </Skeleton>
+            </div>
+            <div className="w-full items-center justify-center flex flex-col gap-2 mt-3">
+                <Skeleton className="h-3 w-3/5 rounded-lg" />
+            </div>
+        </Card>
+
+    );
+}
