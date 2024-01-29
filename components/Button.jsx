5e8120be5a5d62e13065c370fede0a145d67d2d1
druvkotwani <druvkotwani12@gmail.com>
2024-01-29 12:26:32 +0530

    used Link instead of a tag

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Button } from "@nextui-org/react";
 import { GoTo } from "./icons";
+import Link from "next/link";
 
 export default function ButtonComponent({ href }) {
     return (
-        <a href={href} target="_blank" rel="norefferrer" className="flex gap-4 items-center">
+        <Link href={href} target="_blank" rel="norefferrer" className="flex gap-4 items-center">
             <Button href="https://example.com/news" className="h-8 rounded-md" color="default" endContent={<GoTo />} variant="bordered" >
                 <span className="font-medium ">
                     Read News
                 </span>
             </Button>
-        </a>
+        </Link>
     );
 }
