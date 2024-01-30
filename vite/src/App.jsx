1b416cc4f1f792426e6088e46bfbfa80e59fade9
druvkotwani <druvkotwani12@gmail.com>
2024-01-30 21:29:52 +0530

    created vite app

---
+++ vite/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button, ButtonGroup } from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Button color="primary">
+        Button
+      </Button>
+    </h1>
+  )
+}
