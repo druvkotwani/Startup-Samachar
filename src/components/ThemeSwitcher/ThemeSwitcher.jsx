093ccd6153f0681475929f8710cb23572611c7c4
druvkotwani <druvkotwani12@gmail.com>
2024-01-31 00:59:13 +0530

    theme switcher done

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,13 +1,29 @@
 import { Switch } from "@nextui-org/react";
 import { MoonIcon } from "../icons/Icons";
 import { SunIcon } from "../icons/Icons";
-
+import { useEffect, useState } from "react";
 export default function App() {
+    const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
+
+    const handleToggle = (e) => {
+        if (e.target.checked) {
+            setTheme('light')
+        } else {
+            setTheme('dark')
+        }
+    }
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme);
+        const localTheme = localStorage.getItem('theme');
+        document.querySelector('html').setAttribute('data-theme', localTheme);
+    }, [theme])
     return (
         <Switch
             defaultSelected
-            size=""
+            size="md"
             color="secondary"
+            onChange={handleToggle}
             thumbIcon={({ isSelected, className }) =>
                 isSelected ? (
                     <SunIcon className={className} />
